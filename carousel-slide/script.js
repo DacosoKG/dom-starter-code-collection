edf5c932a8cbbbe73578ba8fb4ebc3d91c7a9cab
Azamat Gaparov <agaparovkg@gmail.com>
2024-08-29 14:04:54 -0700

    Updated carousel images

---
+++ carousel-slide/script.js
@@ -0,0 +1,62 @@
+const carouselData = [
+  {
+    name: "high-back bench",
+    image: "https://course-api.com/images/store/product-1.jpeg",
+    id: "rec43w3ipXvP28vog",
+  },
+  {
+    name: "albany table",
+    image: "https://course-api.com/images/store/product-2.jpeg",
+    id: "rec4f2RIftFCb7aHh",
+  },
+  {
+    name: "accent chair",
+    image: "https://course-api.com/images/store/product-3.jpeg",
+    id: "rec8kkCmSiMkbkiko",
+  },
+  {
+    name: "wooden table",
+    image: "https://course-api.com/images/store/product-4.jpeg",
+    id: "recBohCqQsot4Q4II",
+  },
+  {
+    name: "dining table",
+    image: "https://course-api.com/images/store/product-5.jpeg",
+    id: "recDG1JRZnbpRHpoy",
+  },
+  {
+    name: "sofa set",
+    image: "https://course-api.com/images/store/product-6.jpeg",
+    id: "recNWGyP7kjFhSqw3",
+  },
+  {
+    name: "modern bookshelf",
+    image: "https://course-api.com/images/store/product-7.jpeg",
+    id: "recZEougL5bbY4AEx",
+  },
+  {
+    name: "emperor bed",
+    image: "https://course-api.com/images/store/product-8.jpeg",
+    id: "recjMK1jgTb2ld7sv",
+  },
+  {
+    name: "utopia sofa",
+    image: "https://course-api.com/images/store/product-9.jpeg",
+    id: "recmg2a1ctaEJNZhu",
+  },
+  {
+    name: "entertainment center",
+    image: "https://course-api.com/images/store/product-10.jpeg",
+    id: "recvKMNR3YFw0bEt3",
+  },
+  {
+    name: "albany sectional",
+    image: "https://course-api.com/images/store/product-11.jpeg",
+    id: "recxaXFy5IW539sgM",
+  },
+  {
+    name: "leather sofa",
+    image: "https://course-api.com/images/store/product-12.jpeg",
+    id: "recyqtRglGNGtO4Q5",
+  },
+];
