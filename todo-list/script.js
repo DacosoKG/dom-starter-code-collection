ec3e7a7dab44488c7cfe28a5c2c8f6fc7333b1e9
Azamat Gaparov <agaparovkg@gmail.com>
2024-09-02 15:59:16 -0700

    Added todo list starter code

---
+++ todo-list/script.js
@@ -0,0 +1,4 @@
+const addTodoBtn = document.querySelector("#add-new-todo-btn");
+const newTodoInput = document.querySelector("#new-todo-input");
+const todoListContainer = document.querySelector("main");
+const clearAllTodosBtn = document.querySelector("footer button");
